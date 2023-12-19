1f24e618003fc87db5004ab78b34e8a5f0046edb
Jesús S.P. <shamandul@gmail.com>
2023-12-19 22:02:49 +0100

    modificación

---
+++ src/views/inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="/dist/output.css" rel="stylesheet" />
+</head>
+
+<body>
+  <?php include(__DIR__ . '/nav.php'); ?>
+  <main>
