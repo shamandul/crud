bdc820e3fc5e5a20fabd8ba8867a552cdcb51d3e
Jesús S.P. <shamandul@gmail.com>
2023-12-25 20:30:03 +0100

    cambios en rutas y htacces

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
-$entidad = $_GET['e'];
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$entidad = $_GET['e'] ?? 'productos';
+$accion = $_GET['a'] ?? 'listado';
+
+if (!is_dir('src/views/' . $entidad)) {
+    $entidad = 'productos';
+}
+$archivo = 'listado.php';
+if (in_array($accion, ['alta', 'edit'])) {
+    $archivo = 'form.php';
+}
+
 include('src/views/inc/header.php');
-include('src/views/'.$entidad.'/listado.php');
+include('src/views/' . $entidad . '/' . $archivo);
 include('src/views/inc/footer.php');
